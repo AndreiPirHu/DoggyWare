57e4503ea4158a1444e01e5ef2c92f8a914eb7e3
Andrei Pirogov Huertas <andrei.pirogov.huertas@gmail.com>
2024-03-23 12:11:26 +0100

    remove unused imports from navbar, added dependency for router links

--- src/app/layout/mobile-side-menu/mobile-side-menu.component.ts
+++ src/app/layout/mobile-side-menu/mobile-side-menu.component.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-mobile-side-menu',
   standalone: true,
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './mobile-side-menu.component.html',
-  styleUrl: './mobile-side-menu.component.css'
+  styleUrl: './mobile-side-menu.component.css',
 })
 export class MobileSideMenuComponent {
+  sideMenuIsActive: boolean = false;
 
+  handleSideMenuToggle = () => {
+    this.sideMenuIsActive = !this.sideMenuIsActive;
+  };
+
+  handleNavigationClick = () => {
+    this.handleSideMenuToggle();
+  };
 }
--- src/app/layout/navbar-menu/navbar-menu.component.ts
+++ src/app/layout/navbar-menu/navbar-menu.component.ts
@@ -1,17 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { DataService } from '../../data/data.service';
-import { Dog } from '../../data/dog.model';
 import { StateService } from '../../state/state.service';
 import { RouterModule } from '@angular/router';
-import { Employee } from '../../data/employee.model';
-import { Changelog } from '../../data/changelog.model';
 import { SearchBarComponent } from '../search-bar/search-bar.component';
 import { CommonModule } from '@angular/common';
+import { MobileSideMenuComponent } from '../mobile-side-menu/mobile-side-menu.component';
 
 @Component({
   selector: 'app-navbar-menu',
   standalone: true,
-  imports: [RouterModule, SearchBarComponent, CommonModule],
+  imports: [
+    RouterModule,
+    SearchBarComponent,
+    MobileSideMenuComponent,
+    CommonModule,
+  ],
   templateUrl: './navbar-menu.component.html',
   styleUrl: './navbar-menu.component.css',
 })
@@ -19,10 +22,15 @@ export class NavbarMenuComponent implements OnInit {
   searchBarIsActive: boolean = false;
   mobileSideMenuIsActive: boolean = false;
 
+  @ViewChild(MobileSideMenuComponent) mobileSideMenuComponent:
+    | MobileSideMenuComponent
+    | undefined;
+
   constructor(
     private dataService: DataService,
     private stateService: StateService
   ) {}
+
   //sidemenu fetches the data when it appears
   async ngOnInit(): Promise<void> {
     //get the data from the database
@@ -35,6 +43,10 @@ export class NavbarMenuComponent implements OnInit {
       this.stateService.setChangelogs(data.changelogData);
     }
   }
+
+  handleSideMenuChildToggle = () => {
+    this.mobileSideMenuComponent?.handleSideMenuToggle();
+  };
 
   handleSearchBarToggle = () => {
     this.searchBarIsActive = !this.searchBarIsActive;
